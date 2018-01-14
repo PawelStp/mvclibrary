7f52d41ba91d991a27f00b86407969e10c12fd50
NotMeBtw <matyszxzx@gmail.com>
2018-01-14 20:59:48 +0100

    Changed content to polish

--- MVCLibrary/ViewModels/EditUserViewModel.cs
+++ MVCLibrary/ViewModels/EditUserViewModel.cs
@@ -11,17 +11,26 @@ namespace MVCLibrary.ViewModels
     {
 
         public int Id { get; set; }
+
+        [Display(Name = "Imię")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
+
         public string Email { get; set; }
   
+        [Display(Name = "Limit wypożyczeń")]
         public Nullable<int> BorrowLimit { get; set; }
+
+        [Display(Name = "Limit oczekiwania")]
         public Nullable<int> WaitLimit { get; set; }
 
         [Display(Name = "Rola")]
         [Required]
         public string Role { get; set; }
 
+        [Display(Name = "Role")]
         public IEnumerable<SelectListItem> Rols { get; set; }
     }
 }
